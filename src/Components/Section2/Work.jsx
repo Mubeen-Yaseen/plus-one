ace8b9f406db86630bd0a2b842c8f8577d065224
Mubeen Yaeen <mubeenyaseen7257287@gmail.com>
2025-06-08 01:32:04 +0500

    Update

--- src/Components/Section2/Work.jsx
+++ src/Components/Section2/Work.jsx
@@ -6,15 +6,25 @@ import Team from '../../assets/team.svg'
 import Calender from '../../assets/calender.svg'
 
 const Work = () => {
+  const childVariant = {
+    hidden: { opacity: 0, y: -30 }, // animate from top
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.6,
+      },
+    },
+  }
   return (
     <>
       <div className='relative w-full h-screen bg-[#FFFFFF] my-16 md:my-0'>
         <div className='w-full h-[454px] relative mx-auto bg-[#F9FAFC]'>
           <motion.h1
             className="font-poppins font-medium text-[56px] leading-[61.6px] text-center"
-            initial={{ opacity: 0, y: -50 }} // from top
+            initial={{ opacity: 0, y: -100 }} // from top
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 3, ease: 'easeOut' }}
+            transition={{ duration: 4, ease: 'easeOut' }}
             viewport={{ once: true }}
           >
             How it works
@@ -24,7 +34,7 @@ const Work = () => {
 
             {/* Left Card with Animation - Edited Text */}
             <motion.div
-              initial={{ opacity: 0, x: -50 }}
+              initial={{ opacity: 0, x: -100 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 1 }}
               className="w-[350px] h-[230px] pt-[30px] pl-[30px] pb-[30px] bg-white rounded-[16px] shadow-lg "
@@ -49,16 +59,16 @@ const Work = () => {
 
             {/* Right Card with Animation */}
             <motion.div
-              initial={{ opacity: 0, x: 50 }}
+              initial={{ opacity: 0, x: 100 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ duration: 1 }}
               className="w-[350px] h-[230px] p-[30px] bg-white rounded-[16px] shadow-lg"
             >
               <img src={Calender} alt="Icon" />
               <h1 className='text-[20px] py-[15px]'>Enjoy Your Event</h1>
-              <p className='text-[13px] text-[#535353]'>
+              <motion.p className='text-[13px] text-[#535353]' variants={childVariant}>
                 Attend your function with confidence, knowing you have a compatible companion by your side.
-              </p>
+              </motion.p>
             </motion.div>
           </div>
         </div>
